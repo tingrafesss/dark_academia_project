c38bf19937a6ab12fbe75c708dc9a553373d1579
tingrafesss <tingrafes@gmail.com>
2026-04-02 16:16:21 +0300

    Add files via upload

---
+++ dark_academia_project/manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dark_academia.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Не удалось импортировать Django. Убедитесь, что он установлен и "
+            "доступен в вашем виртуальном окружении."
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
