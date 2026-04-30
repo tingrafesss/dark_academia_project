8c6757fa273276bcc7d0ae5ebf338bfe4db0d230
tingrafesss <tingrafes@gmail.com>
2026-04-30 13:31:16 +0300

    Update urls.py

--- dark_academia_project/urls.py
+++ dark_academia_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Сайт работает, приложение core пока отключено для отладки.")
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', home, name='home'),
+    # path('', include('core.urls')),
 ]
