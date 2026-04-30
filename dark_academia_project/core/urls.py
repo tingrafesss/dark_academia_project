f1417087286ff679d2a6bbbb4bd3cb9fb2531a1d
tingrafesss <tingrafes@gmail.com>
2026-04-30 13:19:08 +0300

    Update urls.py

--- dark_academia_project/core/urls.py
+++ dark_academia_project/core/urls.py
@@ -2,25 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
 
-    # path('about/', views.about, name='about'),
-    # path('about-site/', views.about_site, name='about_site'),
+    path('about/', views.about, name='about'),
+    path('about-site/', views.about_site, name='about_site'),
 
-    # path('books/', views.books, name='books'),
-    # path('books/taina-istoria/', views.book_taina, name='book_taina'),
-    # path('books/devyatyi-dom/', views.book_devyatyi_dom, name='book_devyatyi_dom'),
-    # path('books/esli-by-my-byli-zlodeyami/', views.book_zlodei, name='book_zlodei'),
-    # path('books/shesterka-atlasa/', views.book_shesterka, name='book_shesterka'),
+    path('books/', views.books, name='books'),
+    path('books/taina-istoria/', views.book_taina, name='book_taina'),
+    path('books/devyatyi-dom/', views.book_devyatyi_dom, name='book_devyatyi_dom'),
+    path('books/esli-by-my-byli-zlodeyami/', views.book_zlodei, name='book_zlodei'),
+    path('books/shesterka-atlasa/', views.book_shesterka, name='book_shesterka'),
 
-    # path('authors/', views.authors, name='authors'),
-    # path('authors/donna-tartt/', views.author_donna, name='author_donna'),
-    # path('authors/ml-rio/', views.author_rio, name='author_rio'),
-    # path('authors/leigh-bardugo/', views.author_bardugo, name='author_bardugo'),
-    # path('authors/olivie-blake/', views.author_blake, name='author_blake'),
+    path('authors/', views.authors, name='authors'),
+    path('authors/donna-tartt/', views.author_donna, name='author_donna'),
+    path('authors/ml-rio/', views.author_rio, name='author_rio'),
+    path('authors/leigh-bardugo/', views.author_bardugo, name='author_bardugo'),
+    path('authors/olivie-blake/', views.author_blake, name='author_blake'),
 
-    # path('news/', views.news, name='news'),
-    # path('news/reading-start/', views.article_reading, name='article_reading'),
-    # path('news/adaptations/', views.article_adaptations, name='article_adaptations'),
-    # path('news/playlist/', views.article_playlist, name='article_playlist'),
+    path('news/', views.news, name='news'),
+    path('news/reading-start/', views.article_reading, name='article_reading'),
+    path('news/adaptations/', views.article_adaptations, name='article_adaptations'),
+    path('news/playlist/', views.article_playlist, name='article_playlist'),
 ]
